d04faa40b533ef074f8adbf2a6e663cc4daccfe9
scriptura-plus <sergeylyagovich@gmail.com>
2026-04-12 11:57:06 -0400

    Update books.ts

--- src/lib/bible/books.ts
+++ src/lib/bible/books.ts
@@ -2,6 +2,7 @@ export type BibleBook = {
   id: string;
   name: string;
   title: string;
+  shortTitle: string;
   chapters: number;
   versesPerChapter: readonly number[];
   verseCounts: number[];
@@ -11,396 +12,462 @@ const rawBibleBooks = [
   {
     id: "genesis",
     name: "Genesis",
+    shortTitle: "Ge",
     chapters: 50,
     versesPerChapter: [31,25,24,26,32,22,24,22,29,32,32,20,18,24,21,16,27,33,38,18,34,24,20,67,34,35,46,22,35,43,55,32,20,31,29,43,36,30,23,23,57,38,34,34,28,34,31,22,33,26]
   },
   {
     id: "exodus",
     name: "Exodus",
+    shortTitle: "Ex",
     chapters: 40,
     versesPerChapter: [22,25,22,31,23,30,25,32,35,29,10,51,22,31,27,36,16,27,25,26,37,30,33,18,40,37,21,43,46,38,18,35,23,35,35,38,29,31,43,38]
   },
   {
     id: "leviticus",
     name: "Leviticus",
+    shortTitle: "Le",
     chapters: 27,
     versesPerChapter: [17,16,17,35,19,30,38,36,24,20,47,8,59,57,33,34,16,30,37,27,24,33,44,23,55,46,34]
   },
   {
     id: "numbers",
     name: "Numbers",
+    shortTitle: "Nu",
     chapters: 36,
     versesPerChapter: [54,34,51,49,31,27,89,26,23,36,35,16,33,45,41,50,13,32,22,29,35,41,30,25,18,65,23,31,40,16,54,42,56,29,34,13]
   },
   {
     id: "deuteronomy",
     name: "Deuteronomy",
+    shortTitle: "De",
     chapters: 34,
     versesPerChapter: [46,37,29,49,33,25,26,20,29,22,32,31,19,29,23,22,20,22,21,20,23,30,25,22,19,19,26,68,29,20,30,52,29,12]
   },
   {
     id: "joshua",
     name: "Joshua",
+    shortTitle: "Jos",
     chapters: 24,
     versesPerChapter: [18,24,17,24,15,27,26,35,27,43,23,24,33,15,63,10,18,28,51,9,45,34,16,33]
   },
   {
     id: "judges",
     name: "Judges",
+    shortTitle: "Jg",
     chapters: 21,
     versesPerChapter: [36,23,31,24,31,40,25,35,57,18,40,15,25,20,20,31,13,31,30,48,25]
   },
   {
     id: "ruth",
     name: "Ruth",
+    shortTitle: "Ru",
     chapters: 4,
     versesPerChapter: [22,23,18,22]
   },
   {
     id: "1-samuel",
     name: "1 Samuel",
+    shortTitle: "1Sa",
     chapters: 31,
     versesPerChapter: [28,36,21,22,12,21,17,22,27,27,15,25,23,52,35,23,58,30,24,42,15,23,29,22,44,25,12,25,11,31,13]
   },
   {
     id: "2-samuel",
     name: "2 Samuel",
+    shortTitle: "2Sa",
     chapters: 24,
     versesPerChapter: [27,32,39,12,25,23,29,18,13,19,27,31,39,33,37,23,29,33,43,26,22,51,39,25]
   },
   {
     id: "1-kings",
     name: "1 Kings",
+    shortTitle: "1Ki",
     chapters: 22,
     versesPerChapter: [53,46,28,34,18,38,51,66,28,29,43,33,34,31,34,34,24,46,21,43,29,53]
   },
   {
     id: "2-kings",
     name: "2 Kings",
+    shortTitle: "2Ki",
     chapters: 25,
     versesPerChapter: [18,25,27,44,27,33,20,29,37,36,21,21,25,29,38,20,41,37,37,21,26,20,37,20,30]
   },
   {
     id: "1-chronicles",
     name: "1 Chronicles",
+    shortTitle: "1Ch",
     chapters: 29,
     versesPerChapter: [54,55,24,43,26,81,40,40,44,14,47,41,14,17,29,43,27,17,19,8,30,19,32,31,31,32,34,21,30]
   },
   {
     id: "2-chronicles",
     name: "2 Chronicles",
+    shortTitle: "2Ch",
     chapters: 36,
     versesPerChapter: [17,18,17,22,14,42,22,18,31,19,23,16,22,15,19,14,19,34,11,37,20,12,21,27,28,23,9,27,36,27,21,33,25,33,27,23]
   },
   {
     id: "ezra",
     name: "Ezra",
+    shortTitle: "Ezr",
     chapters: 10,
     versesPerChapter: [11,70,13,24,17,22,28,36,15,44]
   },
   {
     id: "nehemiah",
     name: "Nehemiah",
+    shortTitle: "Ne",
     chapters: 13,
     versesPerChapter: [11,20,32,23,19,19,73,18,38,39,36,47,31]
   },
   {
     id: "esther",
     name: "Esther",
+    shortTitle: "Es",
     chapters: 10,
     versesPerChapter: [22,23,15,17,14,14,10,17,32,3]
   },
   {
     id: "job",
     name: "Job",
+    shortTitle: "Job",
     chapters: 42,
     versesPerChapter: [22,13,26,21,27,30,21,22,35,22,20,25,28,22,35,22,16,21,29,29,34,30,17,25,6,14,23,28,25,31,40,22,33,37,16,33,24,41,30,24,34,17]
   },
   {
     id: "psalms",
     name: "Psalms",
+    shortTitle: "Ps",
     chapters: 150,
     versesPerChapter: [6,12,8,8,12,10,17,9,20,18,7,8,6,7,5,11,15,50,14,9,13,31,6,10,22,12,14,9,11,12,24,11,22,22,28,12,40,22,13,17,13,11,5,26,17,11,9,14,20,23,19,9,6,7,23,13,11,11,17,12,8,12,11,10,13,20,7,35,36,5,24,20,28,23,10,12,20,72,13,19,16,8,18,12,13,17,7,18,52,17,16,15,5,23,11,13,12,9,9,5,8,28,22,35,45,48,43,13,31,7,10,10,9,8,18,19,2,29,176,7,8,9,4,8,5,6,5,6,8,8,3,18,3,3,21,26,9,8,24,13,10,7,12,15,21,10,20,14,9,6]
   },
   {
     id: "proverbs",
     name: "Proverbs",
+    shortTitle: "Pr",
     chapters: 31,
     versesPerChapter: [33,22,35,27,23,35,27,36,18,32,31,28,25,35,33,33,28,24,29,30,31,29,35,34,28,28,27,28,27,33,31]
   },
   {
     id: "ecclesiastes",
     name: "Ecclesiastes",
+    shortTitle: "Ec",
     chapters: 12,
     versesPerChapter: [18,26,22,16,20,12,29,17,18,20,10,14]
   },
   {
     id: "song-of-solomon",
     name: "Song of Solomon",
+    shortTitle: "Ca",
     chapters: 8,
     versesPerChapter: [17,17,11,16,16,13,13,14]
   },
   {
     id: "isaiah",
     name: "Isaiah",
+    shortTitle: "Isa",
     chapters: 66,
     versesPerChapter: [31,22,26,6,30,13,25,23,20,34,16,6,22,32,9,14,14,7,25,6,17,25,18,23,12,21,13,29,24,33,9,20,24,17,10,22,38,22,8,31,29,25,28,28,25,13,15,22,26,11,23,15,12,17,13,12,21,14,21,22,11,12,19,12,25,24]
   },
   {
     id: "jeremiah",
     name: "Jeremiah",
+    shortTitle: "Jer",
     chapters: 52,
     versesPerChapter: [19,37,25,31,31,30,34,22,26,25,23,17,27,22,21,21,27,23,15,18,14,30,40,10,38,24,22,17,32,24,40,44,26,22,19,32,21,28,18,16,18,22,13,30,5,28,7,47,39,46,64,34]
   },
   {
     id: "lamentations",
     name: "Lamentations",
+    shortTitle: "La",
     chapters: 5,
     versesPerChapter: [22,22,66,22,22]
   },
   {
     id: "ezekiel",
     name: "Ezekiel",
+    shortTitle: "Eze",
     chapters: 48,
     versesPerChapter: [28,10,27,17,17,14,27,18,11,22,25,28,23,23,8,63,24,32,14,49,32,31,49,27,17,21,36,26,21,26,18,32,33,31,15,38,28,23,29,49,26,20,27,31,25,24,23,35]
   },
   {
     id: "daniel",
     name: "Daniel",
+    shortTitle: "Da",
     chapters: 12,
     versesPerChapter: [21,49,30,37,31,28,28,27,27,21,45,13]
   },
   {
     id: "hosea",
     name: "Hosea",
+    shortTitle: "Ho",
     chapters: 14,
     versesPerChapter: [11,23,5,19,15,11,16,14,17,15,12,14,16,9]
   },
   {
     id: "joel",
     name: "Joel",
+    shortTitle: "Joe",
     chapters: 3,
     versesPerChapter: [20,32,21]
   },
   {
     id: "amos",
     name: "Amos",
+    shortTitle: "Am",
     chapters: 9,
     versesPerChapter: [15,16,15,13,27,14,17,14,15]
   },
   {
     id: "obadiah",
     name: "Obadiah",
+    shortTitle: "Ob",
     chapters: 1,
     versesPerChapter: [21]
   },
   {
     id: "jonah",
     name: "Jonah",
+    shortTitle: "Jon",
     chapters: 4,
     versesPerChapter: [17,10,10,11]
   },
   {
     id: "micah",
     name: "Micah",
+    shortTitle: "Mic",
     chapters: 7,
     versesPerChapter: [16,13,12,13,15,16,20]
   },
   {
     id: "nahum",
     name: "Nahum",
+    shortTitle: "Na",
     chapters: 3,
     versesPerChapter: [15,13,19]
   },
   {
     id: "habakkuk",
     name: "Habakkuk",
+    shortTitle: "Hab",
     chapters: 3,
     versesPerChapter: [17,20,19]
   },
   {
     id: "zephaniah",
     name: "Zephaniah",
+    shortTitle: "Zep",
     chapters: 3,
     versesPerChapter: [18,15,20]
   },
   {
     id: "haggai",
     name: "Haggai",
+    shortTitle: "Hag",
     chapters: 2,
     versesPerChapter: [15,23]
   },
   {
     id: "zechariah",
     name: "Zechariah",
+    shortTitle: "Zec",
     chapters: 14,
     versesPerChapter: [21,13,10,14,11,15,14,23,17,12,17,14,9,21]
   },
   {
     id: "malachi",
     name: "Malachi",
+    shortTitle: "Mal",
     chapters: 4,
     versesPerChapter: [14,17,18,6]
   },
   {
     id: "matthew",
     name: "Matthew",
+    shortTitle: "Mt",
     chapters: 28,
     versesPerChapter: [25,23,17,25,48,34,29,34,38,42,30,50,58,36,39,28,27,35,30,34,46,46,39,51,46,75,66,20]
   },
   {
     id: "mark",
     name: "Mark",
+    shortTitle: "Mr",
     chapters: 16,
     versesPerChapter: [45,28,35,41,43,56,37,38,50,52,33,44,37,72,47,20]
   },
   {
     id: "luke",
     name: "Luke",
+    shortTitle: "Lu",
     chapters: 24,
     versesPerChapter: [80,52,38,44,39,49,50,56,62,42,54,59,35,35,32,31,37,43,48,47,38,71,56,53]
   },
   {
     id: "john",
     name: "John",
+    shortTitle: "Joh",
     chapters: 21,
     versesPerChapter: [51,25,36,54,47,71,53,59,41,42,57,50,38,31,27,33,26,40,42,31,25]
   },
   {
     id: "acts",
     name: "Acts",
+    shortTitle: "Ac",
     chapters: 28,
     versesPerChapter: [26,47,26,37,42,15,60,40,43,48,30,25,52,28,41,40,34,28,41,38,40,30,35,27,27,32,44,31]
   },
   {
     id: "romans",
     name: "Romans",
+    shortTitle: "Ro",
     chapters: 16,
     versesPerChapter: [32,29,31,25,21,23,25,39,33,21,36,21,14,23,33,27]
   },
   {
     id: "1-corinthians",
     name: "1 Corinthians",
+    shortTitle: "1Co",
     chapters: 16,
     versesPerChapter: [31,16,23,21,13,20,40,13,27,33,34,31,13,40,58,24]
   },
   {
     id: "2-corinthians",
     name: "2 Corinthians",
+    shortTitle: "2Co",
     chapters: 13,
     versesPerChapter: [24,17,18,18,21,18,16,24,15,18,33,21,14]
   },
   {
     id: "galatians",
     name: "Galatians",
+    shortTitle: "Ga",
     chapters: 6,
     versesPerChapter: [24,21,29,31,26,18]
   },
   {
     id: "ephesians",
     name: "Ephesians",
+    shortTitle: "Eph",
     chapters: 6,
     versesPerChapter: [23,22,21,32,33,24]
   },
   {
     id: "philippians",
     name: "Philippians",
+    shortTitle: "Php",
     chapters: 4,
     versesPerChapter: [30,30,21,23]
   },
   {
     id: "colossians",
     name: "Colossians",
+    shortTitle: "Col",
     chapters: 4,
     versesPerChapter: [29,23,25,18]
   },
   {
     id: "1-thessalonians",
     name: "1 Thessalonians",
+    shortTitle: "1Th",
     chapters: 5,
     versesPerChapter: [10,20,13,18,28]
   },
   {
     id: "2-thessalonians",
     name: "2 Thessalonians",
+    shortTitle: "2Th",
     chapters: 3,
     versesPerChapter: [12,17,18]
   },
   {
     id: "1-timothy",
     name: "1 Timothy",
+    shortTitle: "1Ti",
     chapters: 6,
     versesPerChapter: [20,15,16,16,25,21]
   },
   {
     id: "2-timothy",
     name: "2 Timothy",
+    shortTitle: "2Ti",
     chapters: 4,
     versesPerChapter: [18,26,17,22]
   },
   {
     id: "titus",
     name: "Titus",
+    shortTitle: "Tit",
     chapters: 3,
     versesPerChapter: [16,15,15]
   },
   {
     id: "philemon",
     name: "Philemon",
+    shortTitle: "Phm",
     chapters: 1,
     versesPerChapter: [25]
   },
   {
     id: "hebrews",
     name: "Hebrews",
+    shortTitle: "Heb",
     chapters: 13,
     versesPerChapter: [14,18,19,16,14,20,28,13,28,39,40,29,25]
   },
   {
     id: "james",
     name: "James",
+    shortTitle: "Jas",
     chapters: 5,
     versesPerChapter: [27,26,18,17,20]
   },
   {
     id: "1-peter",
     name: "1 Peter",
+    shortTitle: "1Pe",
     chapters: 5,
     versesPerChapter: [25,25,22,19,14]
   },
   {
     id: "2-peter",
     name: "2 Peter",
+    shortTitle: "2Pe",
     chapters: 3,
     versesPerChapter: [21,22,18]
   },
   {
     id: "1-john",
     name: "1 John",
+    shortTitle: "1Jo",
     chapters: 5,
     versesPerChapter: [10,29,24,21,21]
   },
   {
     id: "2-john",
     name: "2 John",
+    shortTitle: "2Jo",
     chapters: 1,
     versesPerChapter: [13]
   },
   {
     id: "3-john",
     name: "3 John",
+    shortTitle: "3Jo",
     chapters: 1,
     versesPerChapter: [14]
   },
   {
     id: "jude",
     name: "Jude",
+    shortTitle: "Jude",
     chapters: 1,
     versesPerChapter: [25]
   },
   {
     id: "revelation",
     name: "Revelation",
+    shortTitle: "Re",
     chapters: 22,
     versesPerChapter: [20,29,22,11,14,17,17,13,21,11,19,17,18,20,8,21,18,24,21,15,27,21]
   }
